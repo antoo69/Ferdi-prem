3acf24197f7b26d6fdfd2bbca33345fd6f184016
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-16 05:17:34 +0700

    update vctools.py

--- naya/modules/vctools.py
+++ naya/modules/vctools.py
@@ -17,7 +17,6 @@ from pyrogram.raw.functions.messages import GetFullChat
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.types import Message
-
 from . import *
 
 __MODULE__ = "Vctools"
